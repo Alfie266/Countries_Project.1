afd955940e8debd094a57232de0c6a1b28118e24
Alfie266 <alfredobadiah266@gmail.com>
2023-02-15 10:33:22 +0300

    Revised append logic

--- rest_countries.py
+++ rest_countries.py
@@ -1,4 +1,6 @@
 import requests
+# import pandas as pd
+
 
 official_name = []
 num_code = []
@@ -20,12 +22,8 @@ def get_data():
     """Request the data from the online API"""
     countries_url = "https://restcountries.com/v3.1/all"
     response = requests.get(countries_url)
-    if response == 200:
-        response = response.json()
-    else:
-        print("There has been an error in fetching the data.")
     # print(response.json())
-    return response
+    return response.json()
 
 
 def parse_info():
@@ -33,19 +31,48 @@ def parse_info():
     response = get_data()
 
     for i in response:
-        official_name.append(response[i]['name']['official'])
-        num_code.append(response[i]['ccn3'])
-        alpha_code.append(response[i]['cca3'])
-        un_mem.append(response[i]['unMember'])
-        currencies.append(response[i]['currencies'])
-        capital.append(response[i]['capital'])
-        region.append(response[i]['region'])
-        continent.append(response[i]['continents'])
-        timezone.append(response[i]['timezones'])
-        lat_lng.append(response[i]['latlng'])
-        area.append(response[i]['area'])
-        flags.append(response[i]['flags'])
-        population.append(response[i]['population'])
-        demonyms.append(response[i]['demonyms'])
+        official_name.append(i['name']['official'])
+        # num_code.append(i['ccn3'])
+        alpha_code.append(i['cca3'])
+        un_mem.append(i['unMember'])
+        # currencies.append(i['currencies'])
+        # capital.append(i['capital'])
+        region.append(i['region'])
+        continent.append(i['continents'])
+        timezone.append(i['timezones'])
+        lat_lng.append(i['latlng'])
+        area.append(i['area'])
+        flags.append(i['flags'])
+        population.append(i['population'])
+        # demonyms.append(i['demonyms'])
 
-# print(response[0])
+    # print(response[0])
+
+
+def create_dataframe():
+    pass
+
+
+def plot_graphs():
+    pass
+
+
+def plot_maps():
+    pass
+
+
+# parse_info()
+# print(len(official_name))
+# print(len(num_code))
+# print(len(alpha_code))
+# print(len(un_mem))
+# print(len(currencies))
+# print(len(capital))
+# print(len(region))
+# print(len(continent))
+# print(len(timezone))
+# print(len(lat_lng))
+# print(len(area))
+# print(len(flags))
+# print(len(population))
+# print(len(demonyms))
